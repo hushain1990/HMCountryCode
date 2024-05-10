9adba9bbb630105f9a58207d3df8a52c82adb682
HMK <hushain1990@gmail.com>
2024-05-10 17:01:40 +0530

    Add Country Code Fields

---
+++ CountryCodeTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CountryCodeTextField.swift
+//  HMCountryCode
+//
+//  Created by Hushain Khan on 10/05/24.
+//
+
+import SwiftUI
+
+struct CountryCodeTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryCodeTextField()
+}
